afa363008f68011c20d40e0b07921a6324ae4b55
Azizbayli7 <azizbayli7@protonmail.com>
2021-10-18 18:36:35 +0400

    Task added

---
+++ Week-02/Task-04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4)Remove duplicate data algoritmini yazin input: csharpcorner, output: csharpone c
+            Console.WriteLine("Enter a Word: ");
+            string str = Console.ReadLine();
+
+            //str=str.ToLower(); 
+
+            string str2 = "";
+            for (int i = 0; i < str.Length; i++)
+            { 
+                if (!str2.Contains(str[i]))
+                {
+                    str2 += str[i];
+                }
+            }
+            Console.WriteLine(str2);
+        }
+    }
+}
