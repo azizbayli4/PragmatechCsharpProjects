f9eae6e7aa2c1fdaacf27fc045954d20f1470c7f
Azizbayli7 <azizbayli7@protonmail.com>
2021-10-18 16:23:44 +0400

    Task added

---
+++ Week-02/Task-02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2)Reverse algoritmi qurun: input: hello, output: olleh
+
+            Console.WriteLine("Enter a Word: ");
+            string str = Console.ReadLine();
+           
+            Console.WriteLine($"Reverse word is: \n{Reverse(str)}");
+        }
+
+        static string Reverse(string inputStr)
+        {
+            string reverseStr = "";
+            int length = 0;
+            length = inputStr.Length - 1;
+            while (length >= 0)
+            {
+                reverseStr +=  inputStr[length];
+                length--;
+            }
+            return reverseStr;
+        }
+    }
+}
