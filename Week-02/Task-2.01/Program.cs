bf7e67be2b4001f49df931aa151be1d39fa458c3
Azizbayli7 <azizbayli7@protonmail.com>
2021-10-22 18:53:57 +0400

    Task added

---
+++ Week-02/Task-2.01/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace System
+{
+    namespace Task_2._01
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                static int StudentsCount(string groupNumber, Student[] students)
+                {
+                    int sum = 0;
+                    for (int i = 0; i < students.Length; i++)
+                    {
+                        if (groupNumber == students[i].groupNo)
+                            sum++;
+                    }
+                    return sum;
+                }
+
+                Console.Write("Enter how many students you wanna add: ");
+                int studentCount = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter student details\n");
+                Student[] students = new Student[studentCount];
+                try
+                {
+                    for (int i = 0; i < studentCount; i++)
+                    {
+                        Console.WriteLine("Enter fullname: ");
+                        string fullName = Console.ReadLine();
+                        Console.WriteLine("Enter group number: ");
+                        string groupNumber = Console.ReadLine();
+                        Console.WriteLine("Enter age: ");
+                        int age = Convert.ToInt32(Console.ReadLine());
+
+                        students[i] = new Student(fullName, groupNumber, age);
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You entered invalid data");
+                }
+
+
+                Console.Write("\n\nEnter the group number to get count of students: ");
+                string groupNo = Console.ReadLine();
+                Console.WriteLine($"In group {groupNo } studies {StudentsCount(groupNo, students)} student(s) \nThey are: ");
+
+                Console.Write("\n\nWhich student you wanna get birth year, eneter full name: ");
+                string enteredStudent = Console.ReadLine();
+
+                for (int i = 0; i < students.Length; i++)
+                {
+                    if (students[i].fullName==enteredStudent)
+                    {
+                        Console.WriteLine(students[i].GetBirthYear());
+                    }
+                }
+            }
+        }
+
+        public class Student
+        {
+            public string fullName;
+            public string groupNo;
+            public int age;
+            public Student(string fullName, string groupNo, int age)
+            {
+                this.fullName = fullName;
+                this.groupNo = groupNo;
+                this.age = age;
+            }
+            public int GetBirthYear()
+            {
+                DateTime date = DateTime.Today;
+                return date.Year - this.age;
+            }
+        }
+    
+    }
+
+}
