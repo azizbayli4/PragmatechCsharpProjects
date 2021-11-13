1c62e6c5c0b8343324eed7fad518c30cc9d502b6
Azizbayli7 <azizbayli7@protonmail.com>
2021-11-13 18:27:28 +0400

    Task-1 added. But it could be better. SelfNote: Try to upgrade

---
+++ Tasks/Lesson-8/Task01-Student/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task01_Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Ali", "Aliyev", 38);
+            student.Counter();
+            Console.WriteLine("---------------");
+            Student student1 = new Student("Lale", "Velili", 52);
+            student1.Counter();
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; }
+        public string Surname { get; }
+        public int Age { get; }
+        public static int num;
+        private int Total;
+
+        public Student(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+
+            Total = ++num;
+        }
+        public void Counter()
+        {
+            Console.WriteLine($"Initialized {Total} time(s)");
+        }
+    }
+}
