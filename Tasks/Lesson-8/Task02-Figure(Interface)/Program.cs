af7224004e3e426ce15924a5d679a6dc791a04cc
Azizbayli7 <azizbayli7@protonmail.com>
2021-11-13 18:43:16 +0400

    Task02-Figure added

---
+++ Tasks/Lesson-8/Task02-Figure(Interface)/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Task02_Figure_Interface_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rec1 = new Rectangle();
+            rec1.CalculateArea(4, 5);
+            rec1.CalculatePerimeter(4, 5);
+        }
+    }
+    interface IFigure
+    {
+        void CalculateArea(int a, int b);
+        void CalculatePerimeter(int a, int b);
+
+    }
+    class Rectangle : IFigure
+    {
+        public void CalculateArea(int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine("Area of rectangle: " + result);
+        }
+
+        public void CalculatePerimeter(int a , int b)
+        {
+            int result = 2 * (a + b);
+            Console.WriteLine("Perimeter of rectangular is: " + result);
+        }
+    }
+
+    //class Circle : IFigure
+    //{
+    //    public void CalculateArea()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void CalculatePerimeter()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+    //class Triangle : IFigure
+    //{
+    //    public void CalculateArea()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void CalculatePerimeter()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+    //class Square : IFigure
+    //{
+    //    public void CalculateArea()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void CalculatePerimeter()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
