3f8191daddfa15cdcaac27296db093f0612ef9dc
Azizbayli7 <azizbayli7@protonmail.com>
2022-03-27 14:38:55 +0400

    Area created and working on it

--- ASP .Net Core/AspNetCsharp03/AspNetCsharp03/Areas/Admin/Controllers/DashBoardController.cs
+++ ASP .Net Core/AspNetCsharp03/AspNetCsharp03/Areas/Admin/Controllers/DashBoardController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,81 +6,11 @@ using System.Threading.Tasks;
 
 namespace AspNetCsharp03.Areas.Admin.Controllers
 {
-    public class DashBoardController : Controller
+    public class DashboardController : Controller
     {
-        // GET: DashBoard
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
-        }
-
-        // GET: DashBoard/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: DashBoard/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: DashBoard/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: DashBoard/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: DashBoard/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: DashBoard/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: DashBoard/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
